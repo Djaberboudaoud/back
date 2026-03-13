0921ef4f0165f10340cbf17f7815454df9643745
djaber Boudaoud <92674795+Djaberboudaoud@users.noreply.github.com>
2026-03-14 00:31:47 +0100

    Update password.py

--- app/utils/password.py
+++ app/utils/password.py
@@ -4,8 +4,8 @@ pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
 
 
 def hash_password(password: str):
-    return pwd_context.hash(password)
+    return pwd_context.hash(password[:72])
 
 
 def verify_password(password: str, hashed_password: str):
-    return pwd_context.verify(password, hashed_password)
+    return pwd_context.verify(password[:72], hashed_password)
